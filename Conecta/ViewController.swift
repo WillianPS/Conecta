41c5154463e0e7be9ae0dd9576705c161ee39ce0
Willian Pereira da Silva <willianps@brufpe-13.local>
2017-12-06 16:59:52 -0300

    Projeto inicial

---
+++ Conecta/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Conecta
+//
+//  Created by Willian Pereira da Silva on 05/12/17.
+//  Copyright © 2017 Willian Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var IconeCriarGrupo: UIImageView!
+    
+    
+    @IBOutlet weak var IconeEncontrarGrupo: UIImageView!
+    
+    
+    @IBAction func CriarGrupos(_ sender: UIButton) {
+    }
+    
+ 
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
